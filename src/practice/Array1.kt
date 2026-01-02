3909117860ce4d6a959973a71625dbeb01e68189
isaquliyev <isa.quluyev.wip@gmail.com>
2026-01-03 01:07:50 +0400

    - Q2. Shuffle the Array

--- src/practice/Array1.kt
+++ src/practice/Array1.kt
@@ -6,5 +6,14 @@ class Array1 {
         return intArrayOf(*nums, *nums)
     }
 
+    fun shuffle(nums: IntArray, n: Int): IntArray {
+        val arr = IntArray(2 * n)
+        for (i in 0 until n) {
+            arr[2 * i] = nums[i]
+            arr[2 * i + 1] = nums[n + i]
+        }
+
+        return arr
+    }
 
 }
