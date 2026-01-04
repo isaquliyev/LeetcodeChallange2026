a90be82aa87ca1d5f7350dccdbe0c0ccce3da79e
isaquliyev <isa.quluyev.wip@gmail.com>
2026-01-04 12:52:13 +0400

    - Four Divisors

---
+++ src/1390.kt
@@ -0,0 +1,18 @@
+class Solution {
+    fun sumFourDivisors(nums: IntArray): Int {
+        var finalSum : Int = 0
+        for (i in nums) {
+            var count = 0
+            var tempSum = 0
+            for (j in 2 .. kotlin.math.sqrt(i.toDouble()).toInt()) {
+                if (i % j == 0) {
+                    if (i / j == j) count += 1 else count += 2
+                    tempSum += j + i / j
+                }
+            }
+            if (count == 2) finalSum += 1 + tempSum + i
+        }
+
+        return finalSum
+    }
+}
