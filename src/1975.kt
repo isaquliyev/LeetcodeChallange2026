1c16472669e2381b684478fc65ed4ebf477a46f0
isaquliyev <isa.quluyev.wip@gmail.com>
2026-01-05 11:57:51 +0400

    - Maximum matrix sum

---
+++ src/1975.kt
@@ -0,0 +1,20 @@
+import kotlin.math.abs
+
+class Solution {
+    fun maxMatrixSum(matrix: Array<IntArray>): Long {
+        var minMatrix : Int = matrix[0][0]
+        var sum : Long = 0
+        var negativeCounter : Int = 0
+        for (row in matrix) {
+            for (col in row) {
+                if (abs(col) < abs(minMatrix)) minMatrix = col
+                sum += abs(col)
+                if (col < 0) negativeCounter++
+            }
+        }
+        return when {
+            negativeCounter % 2 == 0 -> sum;
+            else -> sum - 2 * abs(minMatrix)
+        }
+    }
+}
