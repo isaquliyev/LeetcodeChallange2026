e3d0efeda7a648fb6b83d70c1b9ff94e1fd8ff63
isaquliyev <isa.quluyev.wip@gmail.com>
2026-01-30 02:50:21 +0400

    - Minimum cost to Convert String I

--- src/2976.kt
+++ src/2976.kt
@@ -12,6 +12,8 @@ class Solution {
 
     val graph: MutableMap<Char, MutableMap<Char, Int>> = mutableMapOf()
 
+    val cache: MutableMap<Char, MutableMap<Char, Long>> = mutableMapOf()
+
     fun fillTheGraph(original: CharArray, changed: CharArray, cost: IntArray): Unit {
         for (i in 0 until original.size) {
             if(graph[original[i]] == null)
@@ -25,11 +27,11 @@ class Solution {
 
 
 
-    fun shortestPath(sourceId: Char, targetId: Char): Long {
+    fun shortestPath(sourceId: Char): MutableMap<Char, Long> {
 
-        if (sourceId == targetId) return 0
+        if (cache.containsKey(sourceId)) return cache[sourceId]!!
 
-        val pq: PriorityQueue<Node> = PriorityQueue<Node>()
+        val pq = PriorityQueue<Node>()
         val dist: MutableMap<Char, Long> = mutableMapOf()
 
         dist[sourceId] = 0
@@ -37,40 +39,36 @@ class Solution {
 
         while (pq.isNotEmpty()) {
             val u = pq.poll()
-
-            if (dist[u.id] != null && u.dist > (dist[u.id] ?: Long.MAX_VALUE)) continue
+            if (u.dist > (dist[u.id] ?: Long.MAX_VALUE)) continue
 
             val edges = graph[u.id] ?: continue
-
             for ((target, cost) in edges) {
-                val newDist: Long = dist[u.id]!! + cost
-                val oldDist: Long = dist[target] ?: Long.MAX_VALUE
-
-                if(newDist < oldDist) {
+                val newDist = dist[u.id]!! + cost
+                val oldDist = dist[target] ?: Long.MAX_VALUE
+                if (newDist < oldDist) {
                     dist[target] = newDist
                     pq.add(Node(target, newDist))
                 }
             }
         }
 
-        return dist[targetId] ?: -1
+        cache[sourceId] = dist
+        return dist
     }
-
 
 
     fun minimumCost(source: String, target: String, original: CharArray, changed: CharArray, cost: IntArray): Long {
 
-        fillTheGraph(original = original, changed = changed, cost = cost)
+        fillTheGraph(original, changed, cost)
 
-        var totalCost: Long = 0
+        var totalCost = 0L
 
-        for (i in 0 until source.length) {
+        for (i in source.indices) {
+            if (source[i] == target[i]) continue
 
-            val sp = shortestPath(source[i], target[i])
-            if (sp == (-1).toLong()) return -1
+            val distMap = shortestPath(source[i])
+            val sp = distMap[target[i]] ?: return -1
             totalCost += sp
-
-
         }
 
         return totalCost
