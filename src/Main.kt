bfd21823f42a28fa1735cc6d74cddd75886c4484
isaquliyev <isa.quluyev.wip@gmail.com>
2026-01-31 14:06:27 +0400

    - All January problems moved to their own folder :)

--- src/Main.kt
+++ src/Main.kt
@@ -1,13 +1,3 @@
 fun main() {
 
-    /*
-        Level-order representation:
-              1
-            /   \
-           7     0
-          / \
-         7  -8
-     */
-
-   // test invocation
 }
