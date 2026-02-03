a6d1b2ccee1c08390c7a7d907dacd3838ff04270
isaquliyev <isa.quluyev.wip@gmail.com>
2026-02-03 11:14:50 +0400

    - Trionic Array I

---
+++ src/february/3637.kt
@@ -0,0 +1,20 @@
+package february
+
+class Solution {
+    fun isTrionic(nums: IntArray): Boolean {
+
+        var flag = 0
+
+        for (i in 1 until nums.size) {
+            flag = if (flag in 0 .. 1 && nums[i] > nums[i - 1])
+                1
+            else if (flag in 1 .. 2 && nums[i] < nums[i - 1])
+                2
+            else if (flag in 2 .. 3 && nums[i] > nums[i - 1])
+                3
+            else return false
+        }
+
+        return flag == 3
+    }
+}
