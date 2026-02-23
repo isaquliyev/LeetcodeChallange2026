03cad4c09b4b067e37477f151e2af204d14c931d
isaquliyev <isa.quluyev.wip@gmail.com>
2026-02-23 11:04:50 +0100

    - Better solution with Bitmask technique

--- src/february/1461.kt
+++ src/february/1461.kt
@@ -6,18 +6,22 @@ import kotlin.math.pow
 class Solution1461 {
     fun hasAllCodes(s: String, k: Int): Boolean {
 
-        val ht = HashSet<String>()
+        val ht = HashSet<Int>()
+        var num = 0
+        val mask = (1 shl k) - 1
 
-        for (i in 0 until s.length - k)
-            ht.add(s.substring(i, i + k))
+        for (i in 0 until s.length) {
+            num = (num shl 1) or (if (s[i] == '1') 1 else 0) and mask
+            if (i > k - 1)ht.add(num)
+        }
 
-        return ht.size == 2.0.pow(k).toInt()
+        return ht.size == mask + 1
     }
 }
 
 fun main() {
     val solution: Solution1461 = Solution1461()
 
-    println(solution.hasAllCodes("0110", 1))
+    println(solution.hasAllCodes("0110", 2))
 
 }
