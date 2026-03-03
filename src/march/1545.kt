3e2b9b02927072a4275915d976c8e0d4e6430023
isaquliyev <isa.quluyev.wip@gmail.com>
2026-03-03 14:25:47 +0100

    - Pure intuition with explanation

--- src/march/1545.kt
+++ src/march/1545.kt
@@ -1,37 +1,51 @@
 package march
 
-import javax.xml.transform.Source
+import kotlin.math.abs
+import kotlin.math.pow
 
 class Solution1545 {
 
+    /// Right now I guess pure math can work. This is not pure intuition because I already submitted correct answer.
+    /// From several experiments I observed that first char is always 0, last one is always 1.
+    /// And the bits inside all of them mathematically possible to find. I write this before solution, so basically I don't
+    /// know how possible is this.
+
     fun findKthBit(n: Int, k: Int): Char {
-        val s = findString(n)
-        //println(s)
-        return s[k - 1]
-    }
+        /// Let's say we have the string n without calculation.
+        /// Totally 2 ^ n - 1 elements are possible. So, 2 ^ n - 2'th element is 1. This is inversion of 0th
+        /// So (2 ^ n - 2 - x)th element would be xth element's inversion.
+        /// Same is valid for (2 ^ (n - m) - 2), this is always 1, except n - m == 1 case. (Note m != n)
+        /// 2 ^ (n - m) - 2 - x th elements always equal to x's inversion
+        /// If we have some number k - 1, we should convert it to this format 2 ^ (n - m) - 2 - x then we get x.
+        /// Then reproduce it while n - m != 1
 
-    fun findString(n: Int): String {
-        if(n == 1) return "0"
+        var m = 0
+        var x = k
+        var counter: Int = 0
 
-        val f = findString(n - 1)
-        //println(f)
+        // k = 3
+        // n = 2
 
-        return  f + "1" + reverseInvert(f)
-    }
-
-    fun reverseInvert(s: String): String {
-        val arr: MutableList<Char> = mutableListOf()
-        for (c in s) {
-            if(c == '1') arr.add('0')
-            else arr.add('1')
+        while (m != n) {
+            if (n - m == 1) return if (counter % 2 == 1) '1' else '0'
+            if (x == 2.0.pow(n - m - 1).toInt()) break
+            if (x > 2.0.pow(n - m - 1).toInt()) {
+                x = 2.0.pow(n - m).toInt() - x
+                counter++
+            }
+            m++
         }
-        //arr.reversed()
-        return arr.reversed().joinToString("")
+        return if (counter % 2 == 1) '0'
+        else '1'
     }
 }
 
 fun main() {
     val solution: Solution1545 = Solution1545()
 
-    println(solution.findKthBit(20, 1048575))
+    println(solution.findKthBit(3, 2))
+//    println(solution.findKthBit(3, 1))
+//    println(solution.findKthBit(4, 11))
+//    println(solution.findKthBit(20, 1048575))
+
 }
