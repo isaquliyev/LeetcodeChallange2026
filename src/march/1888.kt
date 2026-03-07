2be80094d54341d871066a25fc2a07bdd7ab2d45
isaquliyev <isa.quluyev.wip@gmail.com>
2026-03-07 12:23:08 +0100

    - time and memory efficient solution (removed unnecessary lists)

--- src/march/1888.kt
+++ src/march/1888.kt
@@ -11,30 +11,21 @@ class Solution1888 {
     fun minFlips(s: String): Int {
         val n = s.length
         val s2 = s + s
-        val alt1 = mutableListOf<Char>()
-        val alt2 = mutableListOf<Char>()
         var diff1 = 0
         var diff2 = 0
 
-        for (i in 0 until s2.length) {
-            if (i % 2 == 0) {
-                alt1.add('0')
-                alt2.add('1')
-            } else {
-                alt1.add('1')
-                alt2.add('0')
-            }
-        }
+        fun alt1(i: Int) = if (i % 2 == 0) '0' else '1'
+        fun alt2(i: Int) = if (i % 2 == 0) '1' else '0'
 
         var l = 0
         var res = Int.MAX_VALUE
-        for (r in 0 until s2.length) {
-            if (s2[r] != alt1[r]) diff1++
-            if (s2[r] != alt2[r]) diff2++
+        for (r in 0 until 2 * n) {
+            if (s2[r] != alt1(r)) diff1++
+            if (s2[r] != alt2(r)) diff2++
 
             if (r - l + 1 > n) {
-                if (s[l] != alt1[l]) diff1--
-                if (s[l] != alt2[l]) diff2--
+                if (s2[l] != alt1(l)) diff1--
+                if (s2[l] != alt2(l)) diff2--
                 l++
             }
 
@@ -42,8 +33,6 @@ class Solution1888 {
                 res = minOf(res, diff1, diff2)
             }
         }
-
-
         return res
     }
 }
