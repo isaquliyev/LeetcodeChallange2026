f921bfa8f9ceb1175ee27712e0a7971957c556b1
isaquliyev <isa.quluyev.wip@gmail.com>
2026-03-08 12:05:26 +0100

    - Find unique binary string

---
+++ src/march/1980.kt
@@ -0,0 +1,12 @@
+package march
+
+class Solution {
+    fun findDifferentBinaryString(nums: Array<String>): String {
+        val n = nums.size
+        generate(n).forEach { if (it !in nums) return it }
+        throw Exception()
+    }
+
+    fun generate(n: Int): Sequence<String> =
+        (0..n).asSequence().map { it.toString(2).padStart(n, '0') }
+}
