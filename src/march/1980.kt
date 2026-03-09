689b72c762aad283494d9eec2d5b57e894756b1d
isaquliyev <isa.quluyev.wip@gmail.com>
2026-03-09 20:34:43 +0100

    - class rename

--- src/march/1980.kt
+++ src/march/1980.kt
@@ -1,6 +1,6 @@
 package march
 
-class Solution {
+class Solution1980 {
     fun findDifferentBinaryString(nums: Array<String>): String {
         val n = nums.size
         generate(n).forEach { if (it !in nums) return it }
