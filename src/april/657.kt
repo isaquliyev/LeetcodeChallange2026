2868d67ab58ba6fc7ffd0f96892e4a60ce455892
isaquliyev <isa.quluyev.wip@gmail.com>
2026-04-04 23:40:41 +0200

    - Robot Return to Origin

---
+++ src/april/657.kt
@@ -0,0 +1,19 @@
+package april
+
+class Solution657 {
+    fun judgeCircle(moves: String): Boolean {
+        var x = 0
+        var y = 0
+
+        for (move in moves) {
+            when (move) {
+                'U' -> y--
+                'D' -> y++
+                'L' -> x--
+                'R' -> x++
+            }
+        }
+
+        return x == 0 && y == 0
+    }
+}
